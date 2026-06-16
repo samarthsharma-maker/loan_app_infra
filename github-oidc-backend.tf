bb666d4d4efc5008e996a24cd3df28ad9edcd688
Samarth Sharma <samarthsharma@Samarths-MacBook-Air-2.local>
2026-06-16 18:12:40 +0530

    fix

--- github-oidc-backend.tf
+++ github-oidc-backend.tf
@@ -6,7 +6,7 @@ data "aws_iam_policy_document" "backend_oidc_trust" {
 
     principals {
       type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github.arn]
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
     }
 
     condition {
--- github-oidc.tf
+++ github-oidc.tf
@@ -1,14 +1,4 @@
-
-data "tls_certificate" "github" { url = "https://token.actions.githubusercontent.com" }
-
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
-
-  tags = {
-    Name        = "github-oidc-provider"
-    Environment = var.environment
-    Project     = "loanhub"
-  }
+# Reference the existing GitHub OIDC provider
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
 }
